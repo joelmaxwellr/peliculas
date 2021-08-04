8d9c830e4d7198aae03ba9839b91b61065245cb5
Joel Maxwell <joelmaxwellr@gmail.com>
2021-08-04 07:56:24 -0400

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/card_swiper.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Películas En Cines'),
+          elevation: 0,
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.search_outlined))
+          ],
+        ),
+        body: Column(
+          children: [CardSwiper()],
+        ));
+  }
+}
---
+++ lib/widgets/card_swiper.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_card_swipper/flutter_card_swiper.dart';
+
+class CardSwiper extends StatelessWidget {
+  const CardSwiper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: size.height * 0.5,
+      child: Swiper(
+        itemCount: 10,
+        layout: SwiperLayout.STACK,
+        itemWidth: size.width * 0.6,
+        itemHeight: size.height * 0.9,
+        itemBuilder: (_, int index) {
+          return FadeInImage(
+              placeholder: NetworkImage('https://via.placeholder.com/300x400'),
+              image: NetworkImage(
+                  'https://via.placeholder.com/150/0000FF/808080'));
+        },
+      ),
+    );
+  }
+}
